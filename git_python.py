5a3f65ee43e486f819fd51620507a518f7f88dd8
“shima1999” <shima.manasa@gmail.com>
2023-04-02 22:07:20 +0900

    Python kaita

--- git_python.py
+++ git_python.py
@@ -1,3 +1,23 @@
 import math
 ##python don'd use me
 math.sqrt(4)
+"""
+行列の積
+C = A B
+"""
+
+# 行列Aを作る
+a1_lis = [1, 0, 0]
+a2_lis = [0, 2, 0]
+a3_lis = [0, 0, 4] 
+A_matrix=np.matrix([a1_lis, a2_lis, a3_lis])# "np.array"ではなく，"np.matrix"として行列オブジェクトを生成
+
+# 行列Bを作る
+b1_lis = [3, 0, 0]
+b2_lis = [1, 2, 0]
+b3_lis = [1, 0, 4] 
+B_matrix=np.matrix([b1_lis, b2_lis, b3_lis]) #
+
+
+C_matrix = A_matrix @ B_matrix  C = A 
+print(C_matrix)
